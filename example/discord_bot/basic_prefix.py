a0516d48edc7bcc4e36e8570f6ec62ef02c8ae4f
AlexandroBD <79316328+lol219@users.noreply.github.com>
2022-11-26 21:04:33 +0100

    Added a prefixed command example

---
+++ example/discord_bot/basic_prefix.py
@@ -0,0 +1,34 @@
+import discord
+
+import vacefron
+
+from discord.ext import commands
+
+intents= discord.Intents.default()
+
+
+class Bot(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(command_prefix="!",intents=intents, **kwargs)
+        self.vac_api= vacefron.Client()
+        
+
+    #Optional
+    async def close(self):
+        await self.vac_api.close()
+        await super().close()
+
+bot= Bot()   
+intents.message_content= True
+
+@bot.command()
+async def peposign(ctx, text: str):
+    image = await bot.vac_api.peeposign(text)
+    f =  discord.File(await image.read(), "peposign.png")
+    embed= discord.Embed(title="Peposign")
+    embed.set_image(url=image.url)
+    await ctx.send(embed=embed)
+  
+  
+  
+bot.run("")
