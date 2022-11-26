bcce6fefe86b2f771d2823ed28c698eedb6c8cdb
AlexandroBD <79316328+lol219@users.noreply.github.com>
2022-11-26 20:51:47 +0100

    Fixed indents , improved the code quality.

--- example/discord_bot/basic_slash.py
+++ example/discord_bot/basic_slash.py
@@ -2,19 +2,29 @@ import discord
 from discord import app_commands
 import vacefron
 
-bot= commands.Bot(command_prefix="!", intents= discord.Intents.default())
-intents.message_content= True
+from discord.ext import commands
 
-vacefron = vacefron.Client()
 
+class Bot(commands.Bot):
+    def __init__(self, **kwargs):
+        super().__init__(command_prefix="!",intents= discord.Intents.default(), **kwargs)
+        self.vac_api= vacefron.Client()
+
+    #Optional
+    async def close(self):
+        await self.vac_api.close()
+        await super().close()
+
+bot= Bot()   
 
 @bot.tree.command()
 async def peposign(interaction: discord.Interaction, text: str):
-  image = await vacefron.vac_api.peeposign(text)
-  f =  discord.File(await image.read(), "peposign.png")
-  embed= discord.Embed(title="Peposign")
-  embed.set_image(url=image.url)
-  await interaction.response.send_message(embed=embed)
+    image = await bot.vac_api.peeposign(text)
+    f =  discord.File(await image.read(), "peposign.png")
+    embed= discord.Embed(title="Peposign")
+    embed.set_image(url=image.url)
+    await interaction.response.send_message(embed=embed)
+    # If it doesn't work then you should add await interaction.response.defer() then replace interaction.response.send_message with interaction.followup.send
   
   
-bot.run(token)
+bot.run("")
